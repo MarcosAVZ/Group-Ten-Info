9b1531205022fe1c46fdad361b588d3b8c0cf719
MarcosAVZ <marcosavanzatti@gmial.com>
2024-10-25 13:49:47 -0300

    Ultimando cambios

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -23,8 +23,7 @@ urlpatterns = [
 	path('comentario/eliminar/<int:pk>/', views.Eliminar_Comentario, name='eliminar_comentario'),
 
     path('denunciar/<int:noticia_id>/', views.Denunciar_Noticia, name='denunciar_noticia'),
+    
     path('denuncia_exitosa/', views.denuncia_exitosa, name='denuncia_exitosa'),  # URL para denunciar noticia
-      
-   
 ]
 
